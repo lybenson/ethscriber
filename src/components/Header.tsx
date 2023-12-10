1af7259a84a52b5b780c0ae3be96b79beb43b5cc
lybenson <gwanbit@gmail.com>
2023-12-10 20:52:34 +0800

    feat: add github icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link'
 import ConnectButton from './ConnectButton'
 import ThemeSwitcher from './ThemeSwitcher'
+import GithubIcon from './icons/Github'
 
 export default function Header() {
   return (
@@ -10,6 +12,13 @@ export default function Header() {
 
       <div className='flex gap-2'>
         <ConnectButton />
+        <Link
+          href='https://github.com/lybenson/ethscriber'
+          target='_blank'
+          className='flex items-center'
+        >
+          <GithubIcon width={'2rem'} />
+        </Link>
         <ThemeSwitcher />
       </div>
     </header>
